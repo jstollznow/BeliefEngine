c072e622c5a309ca5500385a0afe189a3afaf218
jacob <jstollznow@hotmail.com>
2019-05-26 00:55:57 +0200

    comments done

--- Program.cs
+++ Program.cs
@@ -25,9 +25,18 @@ namespace BeliefEngine
                         ME.TELL(test);
                         ME.listSentences();
                         bool[] propValues = new bool[] {true, true};
+                        // propositions in globalProps are a list of letters
+                        // the letters are set in the order they are created
+                        // i.e. if you use p then q
+                        // you would send an array of 2 to setPropositions
+                        // array[0] would set p, and array[1] would set q
                         setPropositions(propValues);
                         Console.WriteLine("When " + propositions() + " are equal to " + 
                         propValues.GetValue(0) + " and " + propValues.GetValue(1) + ". Test is " + test.getValue());
+                        // each sentence will account for the globally set values and return an answer
+                        // you could use this method to rotate the values and hence generate a truth table for a 
+                        // given sentence, in the knowledge base, then this truth table could be used
+                        // when trying to add a new sentence
                     break;
                     default:
                         return;
